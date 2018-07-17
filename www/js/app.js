434da248ee32e6923f60e17d1096368fe9ab75fd
Remi Boardman <remi@appware.co.uk>
2018-07-17 17:45:22 +0100

    Demo project initial commit

--- www/js/app.js
+++ www/js/app.js
@@ -7,67 +7,44 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+            }
         }
-      }
+    })
+    .state('app.other', {
+        url: '/other',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/other.html',
+                controller: 'OtherCtrl'
+            }
+        }
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,127 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('HomeCtrl', function($scope, $ionicPlatform, $log) {
+
+    var map; // Google map instance
+    $scope.mapExpanded = false;
+    $scope.mapHeight = {
+        "height" : "35%"
+    };
+
+    $scope.position = {
+        "lat" : "40.748148",
+        "lng" : "-73.985894"
+    }
+
+    $scope.toggleMap = function() {
+
+        if ($scope.mapExpanded) {
+            $scope.mapExpanded = false;
+            $scope.mapHeight = {
+                "height" : "35%"
+            }
+            console.log('mapExpanded: ' + $scope.mapExpanded);
+        }
+        else {
+            $scope.mapExpanded = true;
+            $scope.mapHeight = {
+                "height" : "100%"
+            }
+            console.log('mapExpanded: ' + $scope.mapExpanded);
+        }
+    };
+
+    function loadMap() {
+
+        $log.log('loading map');
+
+        var div = document.getElementById("map-canvas");
+
+        // Initialize the map view
+        map = plugin.google.maps.Map.getMap(div, {
+            'mapType': plugin.google.maps.MapTypeId.ROADMAP,
+            'controls': {
+                'compass': true,
+                'myLocationButton': false,
+                'indoorPicker': true,
+                'zoom': true
+            },
+            'gestures': {
+                'scroll': true,
+                'tilt': true,
+                'rotate': true,
+                'zoom': true
+            },
+            'styles': [
+                {
+                    featureType: "all",
+                    stylers: [
+                        { saturation: -80 }
+                    ]
+                },{
+                    featureType: "road.arterial",
+                    elementType: "geometry",
+                    stylers: [
+                        { hue: "#00ffee" },
+                        { saturation: 50 }
+                    ]
+                },{
+                    featureType: "poi.business",
+                    elementType: "labels",
+                    stylers: [
+                        { visibility: "off" }
+                    ]
+                }
+            ],
+            'camera' : {
+                target: {
+                    lat: $scope.position.lat,
+                    lng: $scope.position.lng
+                },
+                zoom: 10
+            },
+            'preferences': {
+                'zoom': {
+                    'minZoom': 10,
+                    'maxZoom': 18
+                },
+                'building': false
+            }
+        });
+
+        // Wait until the map is ready status.
+        map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+
+        function onMapReady() {
+            $log.log('Map Ready');
+
+            map.addMarker({
+                'position': {
+                    lat: $scope.position.lat,
+                    lng: $scope.position.lng
+                }
+            });
+        }
+    }
+
+    $scope.$on("$ionicView.enter", function(event, data){
+        loadMap();
+    });
+
+    $ionicPlatform.ready(function() {
+        loadMap();
+    });
+
+
+
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('OtherCtrl', function($scope) {
+
+
+})
